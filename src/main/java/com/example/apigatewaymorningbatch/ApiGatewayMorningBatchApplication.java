cfea72d0e5c497b4b9fa5281773f8681376b1670
ramya24-eng <7ramyav@gmail.com>
2024-10-24 16:10:54 +0530

    load balancer and eureka registry

---
+++ src/main/java/com/example/apigatewaymorningbatch/ApiGatewayMorningBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.apigatewaymorningbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayMorningBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayMorningBatchApplication.class, args);
+    }
+
+}
